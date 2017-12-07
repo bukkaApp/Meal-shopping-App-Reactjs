b849915944949f754dd578d3512dbe607f3a54a5
olamiBounty <olamiposi.olorunsola@gmail.com>
2017-12-07 15:42:14 +0100

    user can place order and checkout

--- src/checkout/checkoutSlip.js
+++ src/checkout/checkoutSlip.js
@@ -1,123 +1,138 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import '../style/checkoutSlip.css';
 
-export default class checkoutSlip extends Component{
-	constructor(props) {
-		super(props);
-		this.deleteDiv=this.deleteDiv.bind(this);
-		this.quantityUpdate=this.quantityUpdate.bind(this);
-		this.placeorder=this.placeorder.bind(this);
-		console.log(this.state)
-	} 
+export default class checkoutSlip extends Component {
+  constructor(props) {
+    super(props);
+    this.deleteDiv = this.deleteDiv.bind(this);
+    this.quantityUpdate = this.quantityUpdate.bind(this);
+    this.placeorder = this.placeorder.bind(this);
+    console.log(this.state)
+  }
 
-	deleteDiv(e){
-		this.props.deleteCart(e.target.dataset.key);
-	}
+  deleteDiv(e) {
+    this.props.deleteCart(e.target.dataset.key);
+  }
 
-	quantityUpdate(e) {
-		console.log("the values are ",e.target.value,e.target.dataset.key);
-		this.props.quantityUpdate(e.target.value,e.target.dataset.key);
-	}
+  quantityUpdate(e) {
+    console.log("the values are ", e.target.value, e.target.dataset.key);
+    this.props.quantityUpdate(e.target.value, e.target.dataset.key);
+  }
 
-	async placeorder(){
-		if(!this.props.user.isAuthenticated){
-			alert("You must Sign in first");
-		}
-		else if (this.props.user.isAuthenticated && this.props.chef.fetched_chefsInYourArea) {
-					var chefUid=this.props.chef.yourChef.uid;
-					var customerUid=this.props.user.user.uid
-					var description=""
-					var customerName=this.props.user.user.first_name+" "+this.props.user.user.last_name
-					var customerEmail=this.props.user.user.email
-					var customerImage=this.props.user.user.profile_photo
-					var coupon_used=false;
-					var chefName=this.props.chef.yourChef.first_name+" "+this.props.chef.yourChef.last_name
-					var chefEmail=this.props.chef.yourChef.email
-					var customerAddress=this.props.address.Location
-					var chefImage=this.props.chef.yourChef.profile_photo
-					var customerPhoneNumber=this.props.user.user.mobile
-					var payment_option="cash"
-					var additionalInfo=" "
-					var url="http://salty-escarpment-2400.herokuapp.com/api/v1/bukka/transaction/incoming";
-					var token=this.props.user.user.token;
-					/*Object.keys(this.state.cart.cart).map((menu,key)=>{
+  async placeorder() {
+    if (!this.props.user.isAuthenticated) {
+      alert("You must Sign in first");
+    } else if (this.props.user.isAuthenticated && this.props.chef.fetched_chefsInYourArea) {
+      // var chefUid = this.props.chef.yourChef.uid;
+      var chefUid
+      var customerUid = this.props.user.user.uid
+
+      var description = ""
+      var customerName = this.props.user.user.first_name + " " + this.props.user.user.last_name
+      var customerEmail = this.props.user.user.email
+      var customerImage = this.props.user.user.profile_photo
+      var coupon_used = false;
+      var chefName = this.props.chef.yourChef.first_name + " " + this.props.chef.yourChef.last_name
+      var chefEmail = this.props.chef.yourChef.email
+      var customerAddress = this.props.address.Location
+      var chefImage = this.props.chef.yourChef.profile_photo
+      var customerPhoneNumber = this.props.user.user.mobile
+      var payment_option = "cash"
+      var additionalInfo = " "
+
+      //  url from localhost
+      var url = "http://localhost:4000/api/v1/bukka/transaction/incoming";
+      var token = this.props.user.user.token;
+      /*Object.keys(this.state.cart.cart).map((menu,key)=>{
 						var items=Object.keys(this.state.cart.cart);
 						var item=items[key];
 						var quantity=this.state.cart.cart[`${item}`].quantity;
 						var originalAmt=this.state.cart.cart[`${item}`].totalCost;
 						var charge_customer=false;
 						var change_amount =0;
-						
-						
-					})*/
-						var item="steamed rice"
-						var quantity=1;
-						var originalAmt=200;
-						var charge_customer=true;
-						var change_amount =500;
-					var transaction={
-													chefUid,
-													customerUid,
-													originalAmt,
-													item,
-													customerAddress,
-													description,
-													quantity,
-													customerName,
-													customerEmail,
-													customerImage,
-													chefName,
-													chefEmail,
-													chefImage,
-													customerPhoneNumber,
-													payment_option,
-													coupon_used, 
-													additionalInfo,
-													charge_customer,
-													change_amount 
-												};
-			this.props.placeorder(transaction,token,customerUid,url)
-		}
-}
-	render(){
-		return(
-			<div id="checkoutSlip">
-			<button className="btn-red order-btn" onClick={this.placeorder}>Place Order</button>
-			{Object.keys(this.props.cart.cart).map((key,i)=>{
-				return(
-						<div className="item" key={i}>
-							<div className="carti">
-								<input type="number" onChange={this.quantityUpdate} data-key={key} value={this.props.cart.cart[key].quantity} min="1"/>
-								<h4 className="generalDescription">{key}</h4>
-								<h4 className="cost">₦{Math.round(this.props.cart.cart[key].totalCost*100)/100}</h4>
-							</div>
-							<a className="cancelBtn" data-key={key} onClick={this.deleteDiv}>x</a>
-						</div>
-					)
 
-			})}
-			<input className="add-info" id="chefInfo" placeholder="Add Chef instructions"/>
-			<div>
-				<div className="Totalbreakdown">
-					<h4>Subtotal</h4>
-					<h4 id="subtotal">{"₦"+this.props.cart.total+".00"}</h4>
-				</div>
-				<div className="Totalbreakdown">
-					<h4>Delivery Fee</h4>
-					<h4 id="delivery_charge">{"₦"+(this.props.chef.yourChef.delivery_charge||"0.00")}</h4>
-				</div>
-				<div className="Totalbreakdown">
-					<h4>Tax</h4>
-					<h4 id="tax">₦0.00</h4>
-				</div>
-				
-				<div className="Totalbreakdown">
-					<h2>Total</h2>
-					<h2 id="total">{"₦"+(parseInt(this.props.chef.yourChef.delivery_charge||0)+parseInt(this.props.cart.total))+".00"}</h2>
-				</div>
-				<h6>Promo can only be applied after signing in</h6>
-			</div>
-			</div>
-			)
-	}
+
+					})*/
+      var item = "steamed rice"
+      var quantity = 1;
+      var originalAmt = 200;
+      var charge_customer = true;
+      var change_amount = 500;
+
+
+  // chef uid testing  purpose  change it when on Reall Database
+      var  transaction = [  {
+        chefUid : 'custom:1507371853515olamitest',
+        customerUid,
+        originalAmt,
+        item,
+        customerAddress,
+        description,
+        quantity,
+        customerName,
+        customerEmail,
+        customerImage,
+        chefName,
+        chefEmail,
+        chefImage,
+        customerPhoneNumber,
+        payment_option,
+        coupon_used,
+        additionalInfo,
+        charge_customer,
+        change_amount
+      }]
+
+
+      this.props.placeorder(transaction,token,chefUid, url)
+    }
+  }
+
+
+
+  render() {
+    return (<div id="checkoutSlip">
+      <button className="btn-red order-btn" onClick={this.placeorder}>Place Order</button>
+      {
+        Object.keys(this.props.cart.cart).map((key, i) => {
+          return (<div className="item" key={i}>
+            <div className="carti">
+              <input type="number" onChange={this.quantityUpdate} data-key={key} value={this.props.cart.cart[key].quantity} min="1"/>
+              <h4 className="generalDescription">{key}</h4>
+              <h4 className="cost">₦{Math.round(this.props.cart.cart[key].totalCost * 100) / 100}</h4>
+            </div>
+            <a className="cancelBtn" data-key={key} onClick={this.deleteDiv}>x</a>
+          </div>)
+
+        })
+      }
+      <input className="add-info" id="chefInfo" placeholder="Add Chef instructions"/>
+      <div>
+        <div className="Totalbreakdown">
+          <h4>Subtotal</h4>
+          <h4 id="subtotal">{"₦" + this.props.cart.total + ".00"}</h4>
+        </div>
+        <div className="Totalbreakdown">
+          <h4>Delivery Fee</h4>
+          <h4 id="delivery_charge">{
+              "₦" + (
+              this.props.chef.yourChef.delivery_charge || "0.00")
+            }</h4>
+        </div>
+        <div className="Totalbreakdown">
+          <h4>Tax</h4>
+          <h4 id="tax">₦0.00</h4>
+        </div>
+
+        <div className="Totalbreakdown">
+          <h2>Total</h2>
+          <h2 id="total">{
+              "₦" + (
+              parseInt(this.props.chef.yourChef.delivery_charge || 0) + parseInt(this.props.cart.total)) + ".00"
+            }</h2>
+        </div>
+        <h6>Promo can only be applied after signing in</h6>
+      </div>
+    </div>)
+  }
 }
--- src/routes/checking.js
+++ src/routes/checking.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from '../logo.svg';
 import '../style/App.css';
 import '../style/index.css';
@@ -7,136 +7,165 @@ import CheckoutPage from '../checkout/checkoutPage';
 import CheckoutSlip from '../checkout/checkoutSlip';
 import {connect} from 'react-redux';
 import HeaderCheckout from '../frontpage/HeaderCheckout';
-import { fetch_address, fetch_chef, identify_user, get_chef,showsignIn,showsignUp,updating_user_info,signout,signup,update_cart,showaddCard,addcard} from '../data_Container/action/actions';
+import {
+  fetch_address,
+  fetch_chef,
+  identify_user,
+  get_chef,
+  showsignIn,
+  showsignUp,
+  updating_user_info,
+  signout,
+  signup,
+  update_cart,
+  showaddCard,
+  addcard
+} from '../data_Container/action/actions';
 import SignIn from '../authentication/signIn';
 import SignUp from '../authentication/SignUp';
 import axios from 'axios';
 import AddCard from '../checkout/addCard';
 
-class Checking extends Component{
-	constructor(props){
-		super(props);
-		//this.newUser=this.newUser.bind(this);
-		this.deleteCart=this.deleteCart.bind(this);
-		this.quantityUpdate=this.quantityUpdate.bind(this);
-		this.toggleSignin=this.toggleSignin.bind(this);
-		this.toggleSignUp=this.toggleSignUp.bind(this);
-		this.signin=this.signin.bind(this);
-		this.signout=this.signout.bind(this);
-		this.signup=this.signup.bind(this);
-		this.updateCart=this.updateCart.bind(this);
-		this.toggleShowcard=this.toggleShowcard.bind(this);
-		this.addcard=this.addcard.bind(this);
+class Checking extends Component {
+  constructor(props) {
+    super(props);
+    //this.newUser=this.newUser.bind(this);
+    this.deleteCart = this.deleteCart.bind(this);
+    this.quantityUpdate = this.quantityUpdate.bind(this);
+    this.toggleSignin = this.toggleSignin.bind(this);
+    this.toggleSignUp = this.toggleSignUp.bind(this);
+    this.signin = this.signin.bind(this);
+    this.signout = this.signout.bind(this);
+    this.signup = this.signup.bind(this);
+    this.updateCart = this.updateCart.bind(this);
+    this.toggleShowcard = this.toggleShowcard.bind(this);
+    this.addcard = this.addcard.bind(this);
 
-	}
-	//togglepages
-	toggleSignin(){
-		this.props.dispatch(showsignIn(this.props.page.showsignIn))
-	}
-	toggleSignUp(){
-		this.props.dispatch(showsignUp(this.props.page.showsignUp))
-	}
-	toggleShowcard(){
-		this.props.dispatch(showaddCard(this.props.page.showaddCard))
-	}
-	//addcard
-	addcard(cardNumber,ccv,expirationMonth,expirationYear){
-		var uid=this.props.user.user.uid;
-		var token=this.props.user.user.token;
-		var email=this.props.user.user.email;
-		var url="https://salty-escarpment-2400.herokuapp.com/api/v1/bukka/customer/cardDetails/"+ uid;
-		this.props.dispatch(addcard(axios({ method: 'post',url: url,headers:{token,uid},data:{email,cardNumber,ccv,expirationMonth,expirationYear}})))
-		.then(()=>this.signout())
-		.then(()=>this.toggleShowcard())
-	}
-	//signin and signup
-	signin(email,password){
-		this.props.dispatch(identify_user(axios.post("https://salty-escarpment-2400.herokuapp.com/api/v1/bukka/auth/custom/login",{email,password})))
-		.then(()=>{this.props.dispatch(updating_user_info(axios.get("http://salty-escarpment-2400.herokuapp.com/api/v1/bukka/customer/card/"+this.props.user.user.uid)))})
-		.then(()=>this.toggleSignin())
-	}
+  }
+  //togglepages
+  toggleSignin() {
+    this.props.dispatch(showsignIn(this.props.page.showsignIn))
+  }
+  toggleSignUp() {
+    this.props.dispatch(showsignUp(this.props.page.showsignUp))
+  }
+  toggleShowcard() {
+    this.props.dispatch(showaddCard(this.props.page.showaddCard))
+  }
+  //addcard
+  addcard(cardNumber, ccv, expirationMonth, expirationYear) {
+    var uid = this.props.user.user.uid;
+    var token = this.props.user.user.token;
+    var email = this.props.user.user.email;
+    var url = "https://salty-escarpment-2400.herokuapp.com/api/v1/bukka/customer/cardDetails/" + uid;
+    this.props.dispatch(addcard(axios({
+      method: 'post',
+      url: url,
+      headers: {
+        token,
+        uid
+      },
+      data: {
+        email,
+        cardNumber,
+        ccv,
+        expirationMonth,
+        expirationYear
+      }
+    }))).then(() => this.signout()).then(() => this.toggleShowcard())
+  }
+  //signin and signup
+  signin(email, password) {
+    this.props.dispatch(identify_user(axios.post("https://salty-escarpment-2400.herokuapp.com/api/v1/bukka/auth/custom/login", {email, password}))).then(() => {
+      this.props.dispatch(updating_user_info(axios.get("http://salty-escarpment-2400.herokuapp.com/api/v1/bukka/customer/card/" + this.props.user.user.uid)))
+    }).then(() => this.toggleSignin())
+  }
 
-	signup(email,firstname,lastname,password,mobile,isCustomer){
-		this.props.dispatch(signup(axios.post("https://salty-escarpment-2400.herokuapp.com/api/v1/bukka/auth/register",{email,firstname,lastname,password,mobile,isCustomer})))
-		.then(()=>{this.props.dispatch(identify_user(axios.post("https://salty-escarpment-2400.herokuapp.com/api/v1/bukka/auth/custom/login",{email,password})))})
-		.then(()=>{this.props.dispatch(updating_user_info(axios.get("https://salty-escarpment-2400.herokuapp.com/api/v1/bukka/customer/card/"+this.props.user.user.uid)))})
-		.then(()=>this.toggleSignUp())
-	}
-	//signout
-	signout(){
-		this.props.dispatch(signout());
-	}
-	//updatecart
-	updateCart=(cart)=>{
-		this.props.dispatch(update_cart(cart));
-	}
-	
-	//deleteCartItem
-	deleteCart=(food)=>{
-		var newCart=this.props.cart.cart;
-		delete newCart[food];
-		var cart={};
-		cart.cart=newCart;
-		var total=Object.keys(newCart).map((key,i)=>newCart[key].totalCost).reduce((sum,value)=>sum+value,0).toFixed(2);
-		cart.total=total;
-		this.updateCart(cart);
-	}
-	//quantityUpdate
-	quantityUpdate=(Quantity,key)=>{
-		var price=this.props.cart.cart[key].price,totalCost=price*Quantity,newCart=this.props.cart.cart,cart={};
-		newCart[key].quantity=Quantity;
-		newCart[key].totalCost=totalCost;
-		var total=Object.keys(newCart).map((key,i)=>newCart[key].totalCost).reduce((sum,value)=>sum+value,0).toFixed(2);
-		cart.cart=newCart;
-		cart.total=total;
-		this.updateCart(cart);
-	}
-	//placeorder
-	placeorder=(transaction,token,uid,url)=>{
-		axios({ method: 'post',url: url,headers:{token,uid},body:{transaction:transaction}})
-		.then((r)=>console.log(r))
-		.catch((e)=>console.log("it is me!!!",e))
-	}
-	render(){
-		return(
-			<div className="devi">
-				<HeaderCheckout 	user={this.props.user}
-                            		signout={this.signout}
-                            		cart={this.props.cart} />
-				<PageBackground/> 
-				<CheckoutPage 	newUser={this.newUser} 
-							  	user={this.props.user}
-							  	address={this.props.address}
-							  	toggleSignin={this.toggleSignin}
-							  	toggleSignUp={this.toggleSignUp}
-							  	toggleshowaddcard={this.toggleShowcard} /> 
-				<CheckoutSlip 	cart={this.props.cart}
-								user={this.props.user} 
-								chef={this.props.chef}
-								address={this.props.address} 
-								deleteCart={this.deleteCart} 
-								quantityUpdate={this.quantityUpdate}
-								placeorder={this.placeorder} />
-				{(this.props.page.showsignIn)? <SignIn  toggleSignin={this.toggleSignin}
-   											   			toggleSignUp={this.toggleSignUp}
-   											   			signin={this.signin}
-   											   			user={this.props.user}/>:null}
-   											   			
-				{(this.props.page.showsignUp)? <SignUp  toggleSignUp={this.toggleSignUp}
-											    		toggleSignin={this.toggleSignin}
-											    		signup={this.signup}
-											    		SignUp={this.props.SignUp}
-											    		user={this.props.user}/>:null}
-				{(this.props.page.showaddCard)? <AddCard 	addCard={this.addcard} 
-															user={this.props.user}
-															toggleshowaddcard={this.toggleShowcard}/>:null}
-			</div>
-			)
-	}
+  signup(email, firstname, lastname, password, mobile, isCustomer) {
+    this.props.dispatch(signup(axios.post("https://salty-escarpment-2400.herokuapp.com/api/v1/bukka/auth/register", {
+      email,
+      firstname,
+      lastname,
+      password,
+      mobile,
+      isCustomer
+    }))).then(() => {
+      this.props.dispatch(identify_user(axios.post("https://salty-escarpment-2400.herokuapp.com/api/v1/bukka/auth/custom/login", {email, password})))
+    }).then(() => {
+      this.props.dispatch(updating_user_info(axios.get("https://salty-escarpment-2400.herokuapp.com/api/v1/bukka/customer/card/" + this.props.user.user.uid)))
+    }).then(() => this.toggleSignUp())
+  }
+  //signout
+  signout() {
+    this.props.dispatch(signout());
+  }
+  //updatecart
+  updateCart = (cart) => {
+    this.props.dispatch(update_cart(cart));
+  }
+
+  //deleteCartItem
+  deleteCart = (food) => {
+    var newCart = this.props.cart.cart;
+    delete newCart[food];
+    var cart = {};
+    cart.cart = newCart;
+    var total = Object.keys(newCart).map((key, i) => newCart[key].totalCost).reduce((sum, value) => sum + value, 0).toFixed(2);
+    cart.total = total;
+    this.updateCart(cart);
+  }
+  //quantityUpdate
+  quantityUpdate = (Quantity, key) => {
+    var price = this.props.cart.cart[key].price,
+      totalCost = price * Quantity,
+      newCart = this.props.cart.cart,
+      cart = {};
+    newCart[key].quantity = Quantity;
+    newCart[key].totalCost = totalCost;
+    var total = Object.keys(newCart).map((key, i) => newCart[key].totalCost).reduce((sum, value) => sum + value, 0).toFixed(2);
+    cart.cart = newCart;
+    cart.total = total;
+    this.updateCart(cart);
+  }
+  //placeorder
+  placeorder = (transaction, token, chefUid, url) => {
+    console.log(transaction,chefUid,token)
+    axios({
+      method: 'post',
+      url: url,
+      headers: {token, chefUid },
+      data: {
+           transaction
+      }
+    }).then((r) => console.log(r)).catch((e) => console.log("it is me!!!", e))
+  }
+
+  render() {
+    return (<div className="devi">
+      <HeaderCheckout user={this.props.user} signout={this.signout} cart={this.props.cart}/>
+      <PageBackground/>
+      <CheckoutPage newUser={this.newUser} user={this.props.user} address={this.props.address} toggleSignin={this.toggleSignin} toggleSignUp={this.toggleSignUp} toggleshowaddcard={this.toggleShowcard}/>
+      <CheckoutSlip cart={this.props.cart} user={this.props.user} chef={this.props.chef} address={this.props.address} deleteCart={this.deleteCart} quantityUpdate={this.quantityUpdate} placeorder={this.placeorder}/> {
+        (this.props.page.showsignIn)
+          ? <SignIn toggleSignin={this.toggleSignin} toggleSignUp={this.toggleSignUp} signin={this.signin} user={this.props.user}/>
+          : null
+      }
+
+      {
+        (this.props.page.showsignUp)
+          ? <SignUp toggleSignUp={this.toggleSignUp} toggleSignin={this.toggleSignin} signup={this.signup} SignUp={this.props.SignUp} user={this.props.user}/>
+          : null
+      }
+      {
+        (this.props.page.showaddCard)
+          ? <AddCard addCard={this.addcard} user={this.props.user} toggleshowaddcard={this.toggleShowcard}/>
+          : null
+      }
+    </div>)
+  }
 }
 
-
-function mapStateToProps(state){
-	return state;
+function mapStateToProps(state) {
+  return state;
 };
 export default connect(mapStateToProps)(Checking)
